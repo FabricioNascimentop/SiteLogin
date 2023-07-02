bfae078311f15f59bdbc5d6ee8b784c7eab0fa64
FabricioNascimentop <119469677+FabricioNascimentop@users.noreply.github.com>
2023-07-02 17:56:56 -0300

    commit wow

---
+++ main.py
@@ -0,0 +1,54 @@
+#"head" do flask, importações iniciais, definição da variável "app", da secret kay
+from flask import Flask, render_template, request, redirect, flash
+app = Flask(__name__)
+app.secret_key = 'Fabricio'
+#classe que unifica os dados do usuário, em outras palavras uma conta (futuramente serão guardados em um banco de dados)
+class Usuario:
+    def __init__(self, nome, email, senha):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+
+#página inicial (sem estilização ainda)
+@app.route('/')
+def inicio():
+    return render_template('index.html')
+
+#meramente a página de login (sem estilização)
+@app.route('/login')
+def logar():
+    return render_template('login.html')
+
+#função e rota que autentica a entrada no site
+#validação será melhorada considerando as contas que foram criadas no site
+@app.route('/autenticar login',methods=['POST',])
+def autenticar_login():
+    email = request.form['Email']
+    senha = request.form['SenhaLogin']
+    if email == 'fab' and senha == '123':
+        return render_template('site.html')
+    else:
+        flash('DIGITASTE ALGO ERRADO')
+        return redirect('/login')
+
+#autenticar criação de conta 
+#futuramente autenticará a validade de email
+@app.route('/autenticar conta',methods=['POST',])
+def autenticar_conta():
+    nome = request.form.get("Nomecriarconta")
+    email = request.form.get("Emailcriarconta")
+    senha = request.form.get("Senhacriaconta")
+    #guarda as contas criadas num arquivo txt sem segurança nenhuma
+    #futuramente haverá uma validação de armazenar ou não de acordo com se já tem no arquivo
+    with open ("contas.txt",'a') as contas:
+        contas.write(f"{nome}\n{email}\n{senha}\n")
+    return redirect('/')
+
+#retorna a página de criar conta (sem estilização)
+@app.route('/criar conta')
+def criaconta():
+    return render_template('cria_conta.html')
+
+
+
+app.run(debug=True)
